f22f5023acf87d681ce6cad00b2c1c4bb8e137db
Evan Coury <me@evancoury.com>
2011-08-19 09:01:38 -0700

    Bug fix for sort link.
    After cleaning up the cache I forgot I still needed to pass the sort parameter
    to the view for the sort link.

--- application/modules/zfstatus/controllers/IndexController.php
+++ application/modules/zfstatus/controllers/IndexController.php
@@ -12,13 +12,14 @@ class Zfstatus_IndexController extends Zend_Controller_Action
         }
 
         $this->view->repo = $repos[$repo];
+        $this->view->sort = $this->_getParam('sort');
         $this->view->zfService = Zend_Registry::get('Zfstatus_DiContainer')->getZfService();
         $this->view->gh = Zend_Registry::get('Zfstatus_DiContainer')->getGitHubService();
         
         $cache = Zend_Registry::get('Zfstatus_DiContainer')->getCacheManager()->getCache('default');
-        $cacheTag = 'accordion_' . preg_replace('/[^A-Za-z0-9_]/','',$repo.$this->_getParam('sort'));
+        $cacheTag = 'accordion_' . preg_replace('/[^A-Za-z0-9_]/','',$repo.$this->view->sort);
         if ( ($this->view->recentActivity = $cache->load($cacheTag)) === false ) {
-            $this->view->recentActivity = $this->view->zfService->getRecentActivity($this->view->repo, $this->_getParam('sort'));
+            $this->view->recentActivity = $this->view->zfService->getRecentActivity($this->view->repo, $this->view->sort);
             $cache->save($this->view->recentActivity, $cacheTag);
         }
     }
